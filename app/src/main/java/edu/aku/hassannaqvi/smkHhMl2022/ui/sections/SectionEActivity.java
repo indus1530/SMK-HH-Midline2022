e7d7429fee24744b3e05a8b68995d18bfb4958aa
SAJID LATIF <indus1530@gmail.com>
2022-07-16 14:38:29 +0500

    SectionEActivity Added

---
+++ app/src/main/java/edu/aku/hassannaqvi/smkHhMl2022/ui/sections/SectionEActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.smkHhMl2022.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.smkHhMl2022.R;
+
+public class SectionEActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_e);
+    }
+}
